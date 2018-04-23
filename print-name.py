c1831ed072292ee46fc3d7cb3f682c4aa10e3866
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2018-04-22 21:46:19 -0700

    Added Puthon program

---
+++ print-name.py
@@ -0,0 +1,5 @@
+print("This python program reprints your name");
+name=input("Enter your name here: ");
+print("You have entered "+ name);
+#print("It is of type:"+ typeof(name));
+print("end of program");
