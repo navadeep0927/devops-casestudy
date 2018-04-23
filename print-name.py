d120f109478bb3b08d4796c85ca224704b0f699d
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2018-04-22 22:01:29 -0700

    Review comments incorporated

--- print-name.py
+++ print-name.py
@@ -3,4 +3,5 @@ name=input("Enter your name here: ");
 print("You have entered "+ name);
 print("Lets find your lucky number");
 lucky_number=input("Enter your lucky number: ");
+print("Lucky number is:" + lucky_number);
 print("end of program");
